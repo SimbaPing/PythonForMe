d706ef490243b12e2144b4ea79b3b12f61653466
pjs <fpsping@163.com>
2018-03-01 02:41:25 +0800

    requests_html_2

--- PythonTest/test/requestshtml_1.py
+++ PythonTest/test/requestshtml_1.py
@@ -14,3 +14,16 @@ session = HTMLSession()
 r = session.get('https://python.org/')
 
 print(r.html.links)
+print(r.html.absolute_links)
+about = r.html.find('#about', first=True)
+print(about.text)
+print(about.attrs)
+print(about.html)
+print(about.find('a'))
+print(about.absolute_links)
+print(r.html.search('Python is a {} language')[0])
+r = session.get('https://github.com/')
+sel = 'body > div.application-main > div.jumbotron.jumbotron-codelines > div > div > ' \
+      'div.col-md-7.text-center.text-md-left > p '
+print(r.html.find(sel, first=True).text)
+print(r.html.xpath('a'))
