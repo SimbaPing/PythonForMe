62292bae9d92274e32ae1f937d177ccf37fc6109
pjs <fpsping@163.com>
2018-03-05 23:58:00 +0800

    requeststest2

--- PythonTest/test/requestshtml_1.py
+++ PythonTest/test/requestshtml_1.py
@@ -9,21 +9,40 @@ Created with IntelliJ IDEA
 """
 
 from requests_html import HTMLSession
+from requests_html import HTML
 
 session = HTMLSession()
 r = session.get('https://python.org/')
-
+print(1)
 print(r.html.links)
+print(2)
 print(r.html.absolute_links)
+print(3)
 about = r.html.find('#about', first=True)
 print(about.text)
+print(4)
 print(about.attrs)
+print(5)
 print(about.html)
+print(6)
 print(about.find('a'))
+print(7)
 print(about.absolute_links)
+print(8)
 print(r.html.search('Python is a {} language')[0])
+print(9)
 r = session.get('https://github.com/')
 sel = 'body > div.application-main > div.jumbotron.jumbotron-codelines > div > div > ' \
       'div.col-md-7.text-center.text-md-left > p '
 print(r.html.find(sel, first=True).text)
+print(10)
 print(r.html.xpath('a'))
+print(11)
+r = session.get('http://python-requests.org')
+r.html.render()
+print(r.html.search('Python 2 will retire in only {mouths} mouth!')['mouth'])
+print(12)
+doc = """<a href='https://httpbin.org'>"""
+html = HTML(html=doc)
+print(html.links)
+print('end')
