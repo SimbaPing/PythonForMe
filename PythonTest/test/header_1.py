cac678795a80fb57456ef1c068562c57292c3d83
pjs <fpsping@163.com>
2018-03-07 23:42:40 +0800

    other

---
+++ PythonTest/test/header_1.py
@@ -0,0 +1,12 @@
+print('Hello world!')
+'''
+File: header_1.py
+Project: test
+File Created: Wednesday, 7th March 2018 7:11:13 pm
+Author: pjs (you@you.you)
+-----
+Last Modified: Wednesday, 7th March 2018 7:23:40 pm
+Modified By: pjs (you@you.you>)
+-----
+Copyright 2017 - 2018 Your Company, Your Company
+'''
