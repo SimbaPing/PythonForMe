9c743777d2e35d93f7fef707333ad4ac347196a2
pjs <fpsping@163.com>
2018-06-21 23:57:26 +0800

    other

--- PythonTest/taihe/yg-c.py
+++ PythonTest/taihe/yg-c.py
@@ -8,16 +8,6 @@ Created with IntelliJ IDEA
 @Time: 2018/6/16 17:56
 """
 
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Created with IntelliJ IDEA
-@Author: Ping
-@Contact: fpsping@163.com
-@File: yg1.py
-@Time: 2018/6/6 16:58
-"""
-
 import random
 import xlwt
 
