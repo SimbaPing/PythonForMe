e28aaa5888f0233a6dc69eaeba13e1cefe6a554a
pjs <fpsping@163.com>
2018-07-26 23:37:52 +0800

    class-1

--- PythonTest/pyclass/class-1.py
+++ PythonTest/pyclass/class-1.py
@@ -7,7 +7,7 @@
 .......file: class-1
 .......time: 2018/07/26 21:38:21
 Created with Visual Studio Code.
-description:
+description: 类和调用类
 """
 
 
@@ -21,8 +21,26 @@ class Students(object):
         print('%s: %s' % (self.name, self.score))
 
 
+class TwoStudent(object):
+
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self):
+        return self.__name
+
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+
+
 if __name__ == '__main__':
     zs = Students('张三', 99)
-    ls = Students('李四', 89)
+    ls = TwoStudent('李四', 89)
     zs.print_score()
     ls.print_score()
+    print(zs.name)
+    # print(ls.__name)  # 私有变量无法访问
