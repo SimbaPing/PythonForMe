5263cd493760a625bd9cab379762a979d3ca13c8
pjs <fpsping@163.com>
2018-07-27 23:35:40 +0800

    object-class

---
+++ PythonTest/pyclass/class-2.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.....author: Ping
+....contact: uppjs@qq.com
+.......file: class-2
+.......date: 2018/7/27 22:51
+created with IntelliJ IDEA
+description: 继承
+"""
+
+
+class Animal(object):
+
+    @staticmethod
+    def run():
+        print('Animal is running.')
+
+
+class Dog(Animal):
+
+    def run(self):
+        print('Dog is running.')
+
+    @staticmethod
+    def eat():
+        print('Eating meat.')
+
+
+class Cat(Animal):
+
+    def run(self):
+        print('Cat is running.')
+
+
+class Tortoise(Animal):
+
+    def run(self):
+        print('Tortoise is running slowly.')
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+if __name__ == '__main__':
+    dog = Dog()
+    dog.run()
+    cat = Cat()
+    cat.run()
+    run_twice(Animal())
+    run_twice(Dog())
+    run_twice(Tortoise())
