0f625a787d3ccc3b09875f077e55577d3e73cace
pjs <fpsping@163.com>
2018-08-05 23:48:18 +0800

    other

---
+++ PythonTest/test/二维码二合一.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.....author: Ping
+....contact: uppjs@qq.com
+.......file: 二维码二合一
+.......date: 2018/8/5 22:31
+created with IntelliJ IDEA
+description:
+"""
+
