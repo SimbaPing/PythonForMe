e2b6f695d1194bbaec0a51e712d008a4a45591c9
uppjs <uppjs@qq.com>
2018-10-07 05:19:43 +0800

    leetcode-344-reverse-string

---
+++ PythonLearn/leetcode/arithmetic/t-reverse-string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Created with IntelliJ IDEA
+Created by uppjs on 2018/10/7 5:16
+Filename: t-reverse-string.py
+Contact: uppjs@qq.com
+Description: 翻转字符串
+"""
+
+s1 = "hello world"
+
+
+# def reverseString(s):
+#
+
+print(s1[::-1])
