513b2f2a2bdc7501882357cd0d2846e01071139f
uppjs <uppjs@qq.com>
2018-10-07 04:26:53 +0800

    python-stack

---
+++ Z/Zz/LC/t-stack.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Created with IntelliJ IDEA
+Created by uppjs on 2018/10/7 4:14
+Filename: t-stack.py
+Contact: uppjs@qq.com
+Description: push pop top
+"""
+
+# pop 移除列表中的一个值，并显示
+list1 = ['Google', 'Runoob', 'Taobao']
+
+list_pop = list1.pop(1)
+print(list_pop)
+print(list1)
