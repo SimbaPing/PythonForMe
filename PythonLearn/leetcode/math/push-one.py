fe67e55e99ac4e6477abedc3a1d58c3cc615eb9e
uppjs <uppjs@qq.com>
2018-10-10 13:53:11 +0800

    other

---
+++ PythonLearn/leetcode/math/push-one.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Created with IntelliJ IDEA
+Created by uppjs on 2018/10/7 5:56
+Filename: pust-one.py
+Contact: uppjs@qq.com
+Description: 
+"""
---
+++ Z/Zz/tt/t1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Created with IntelliJ IDEA
+Created by uppjs on 2018/10/10 9:10
+Filename: t1.py
+Contact: uppjs@qq.com
+Description: 
+"""
