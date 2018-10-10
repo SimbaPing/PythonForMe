6a0e675b6b5f6d0f609104a8697399f902e2028e
uppjs <uppjs@qq.com>
2018-10-10 13:53:50 +0800

    other

--- PythonLearn/leetcode/math/push-one.py
+++ PythonLearn/leetcode/math/push-one.py
@@ -4,7 +4,36 @@
 """
 Created with IntelliJ IDEA
 Created by uppjs on 2018/10/7 5:56
-Filename: pust-one.py
+Filename: push-one.py
 Contact: uppjs@qq.com
 Description: 
 """
+
+s = [4, 3, 2, 1]
+
+s1 = s[len(s) - 1] + 1
+del(s[len(s) - 1])
+print(s)
+print(s1)
+s.append(s1)
+print(s)
+
+a = '1234'
+print(list(a))
+number_a = list(map(int, a))  # 数字字符串转数字列表
+# 数字列表转数字
+print(number_a)
+
+b2 = []
+b = [1, 2, 3, 4]
+for i in b:
+    b2.append(str[i])
+    print(b2)
+
+print(b1)
+
+
+'''
+1. 字符串转列表
+2. 列表最后一项 +1
+'''
--- Z/Zz/tt/t1.py
+++ Z/Zz/tt/t1.py
@@ -8,3 +8,52 @@ Filename: t1.py
 Contact: uppjs@qq.com
 Description: 
 """
+
+l1 = [1, 2, 3, 1, 3]
+
+for i in l1:
+    s1 = int(''.join(str(i)))
+print(s1)
+# l1.remove(1)
+# print(l1)
+
+print('****************************************')
+l2 = '1234'
+l20 = []
+for i in l2:
+    l20.append(int(i))
+
+print(list(l2))
+
+print(l20)
+
+l21 = ''.join(list(l2))
+
+
+# 数字型字符串可变为：
+# 1. int(str) - 变为数字
+# 2. list(str) - 变为字符串列表
+# 3. int(i) for i in list(str) - 变为数字列表
+# TODO: [1, 2, 3, 4] ==> 1234
+lc = [1, 2, 3, 4]
+ld = []
+for i in lc:
+    ld.append(str(i))
+print(ld)
+le = ''.join(ld)
+print(int(le))
+print(type(int(le)))
+print(1 + int(le))
+print('****************************************************')
+
+d = []
+for i in lc:
+    d.append(str(i))
+e = ''.join(d)
+f = 1 + int(e)
+g = []
+print(str(f))
+for i in str(f):
+    g.append(int(i))
+print(g)
+
