43b29449475777610c79bcd50c5ae12d623efac3
uppjs <uppjs@qq.com>
2018-10-23 09:25:39 +0800

    other

---
+++ PythonLearn/leetcode/t-count-primes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Created with IntelliJ IDEA
+Created by uppjs on 2018/10/12 10:17
+Filename: t-count-primes.py
+Contact: uppjs@qq.com
+Description: 
+"""
