ca970c2e250f1dd32d10b03e256f6631616e63c2
uppjs <uppjs@qq.com>
2019-03-16 21:17:59 +0800

    .

--- PythonLearn/pyrequests/PyBiLi.py
+++ PythonLearn/pyrequests/PyBiLi.py
@@ -1,5 +1,29 @@
+# coding=utf-8
 import requests
-import os
+import re
 
-url = "https://www.bilibili.com/video/av46115482?from=search&seid=14001123198564363028/"
-root = "C://Users//uppjs//Desktop"
+
+aid = input('请输入av号：')
+# 获取bilibili视频地址
+url = 'https://www.bilibili.com/video/av' + aid
+headers = {'Host': 'www.bilibili.com',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+           'Connection': 'keep-alive'
+           }
+
+res = requests.get(url=url, headers=headers).text
+
+vid = re.compile(r'"url":"http(.+?)","backup_url":.+?')
+tittle = re.findall(r'<h1 title="(.+?)".*?>', res)[0]
+vid_url = 'https' + re.findall(vid, res)[0]
+print(tittle, '\n', vid_url)
+vid_headers = {
+    'Origin': 'https://www.bilibili.com',
+    'Referer': url,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+}
+video = requests.get(url=vid_url, headers=vid_headers).content
+
+with open('./video/' + tittle + '.mp4', 'wb+') as f:
+    f.write(video)
+
