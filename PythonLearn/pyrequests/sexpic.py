ea9c83258b834e1cc2134ab2c5bad675c742e8ab
uppjs <uppjs@qq.com>
2019-04-05 21:53:33 +0800

    .

--- PythonLearn/pyrequests/sexpic.py
+++ PythonLearn/pyrequests/sexpic.py
@@ -3,3 +3,18 @@
 
 # Created with IntelliJ IDEA by Django on 2019/3/19 21:32
 
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = 'http://pic.xxx55tp.com/images/jlkaxklxkj/4887964/002.jpg'
+headers = {
+    'User-Agent': 'Mozilla/4.0(compatible;MSIE 5.5;Windows NT)',
+}
+
+html = requests.get(url=url, stream=True)
+with open('selenium.jpg', 'wb') as file:
+    for data in html.iter_content(128):
+        file.write(data)
+
+print(requests.status_codes)
