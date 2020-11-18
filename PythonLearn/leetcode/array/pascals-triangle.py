f31f867297f99c077beb0b6683d037d304c00e86
pjs <uppjs@qq.com>
2020-11-18 11:06:39 +0800

    2020年11月18日11:06:37

--- PythonLearn/leetcode/array/pascals-triangle.py
+++ PythonLearn/leetcode/array/pascals-triangle.py
@@ -1,5 +1,9 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
+'''
+Date: 2020-11-17 20:16:13
+LastEditors: uppjs@qq.com
+LastEditTime: 2020-11-18 11:05:40
+'''
+
 
 """
 Created with IntelliJ IDEA
---
+++ PythonTest/newtest/newone.py
@@ -0,0 +1,7 @@
+'''
+Date: 2020-11-17 22:30:46
+LastEditors: uppjs@qq.com
+LastEditTime: 2020-11-17 22:31:37
+'''
+
+print("你好good boy!")
